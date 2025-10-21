129a41afd694625405f19a57d766ce6251f08321
IDIC <alumnos@alumnosuio.ute.edu.ec>
2025-10-21 10:01:12 -0500

    clase 2025-10-21

---
+++ modulo2/javascript/04_condicionales.js
@@ -0,0 +1,69 @@
+let temperatura= 35;
+
+if(temperatura>30){
+    console.log("Hace calor");
+}
+
+let password ="pass123";
+if(password==="pass12345"){
+    console.log("Usuario logueado");
+} else {
+    console.log("Acceso denegado");
+}
+
+let nota =7;
+if(nota>=9){
+    console.log("Sobresaliente");
+} else if (nota>=6) {
+    console.log("Aprobado");
+}else {
+    console.log("Reprobado");
+}
+
+let edad=20;
+let tiene_licencia=true;
+if(edad>=18){
+    if(tiene_licencia){
+        console.log("Puede Conducir");
+    }else{
+        console.log("Necesita Licencia para conducir");
+    }
+} else {
+    console.log("Es menor de edad");
+}
+
+edad=18;
+tiene_licencia=true;
+if(edad>=18 &&tiene_licencia){
+        console.log("Puede Conducir");
+}else if(edad>=18) {
+    console.log("Necesita Licencia para conducir");
+}else {
+    console.log("Es menor de edad");
+}
+// Estructura Switch
+let dia = "lunes";
+switch(dia){
+    case "lunes":
+        console.log("Inicio de semana");
+        break;
+    case "viernes":
+        console.log("último dia laboral");
+        break;
+    default:
+        console.log("Día Normal");
+}
+
+let a =30;
+let b = 40;
+let c = 50;
+mayor=0;
+if (a>b) {
+    mayor=a;
+}if (c>mayor){
+    mayor=c;
+}
+console.log("el numero mayor es", mayor);
+
+
+
